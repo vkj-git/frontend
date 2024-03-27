9a8a7aedd17cea53c86759f2b3e5fabac66f6649
vkj-git <99423921+vkj-git@users.noreply.github.com>
2024-03-27 22:40:34 +0530

    Update Login.js

--- src/users/pages/Login.js
+++ src/users/pages/Login.js
@@ -16,7 +16,7 @@ const Login = () => {
     setError(null);
     try {
       const response = await fetch(
-        "https://bckend-server.vercel.app/api/users/login",
+        "https://backend-nine-liard-28.vercel.app/api/users/login",
         {
           //const response = await fetch("http://localhost:5000/api/users/login", {
           method: "POST",
