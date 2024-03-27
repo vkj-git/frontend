eff87126e5d7c89889c4ac5d1ac88a304febd30f
vkj-git <99423921+vkj-git@users.noreply.github.com>
2024-03-27 22:40:19 +0530

    Update Users.js

--- src/users/pages/Users.js
+++ src/users/pages/Users.js
@@ -9,7 +9,7 @@ const Users = () => {
     const sendRequest = async () => {
       try {
         const response = await fetch(
-          "https://bckend-server.vercel.app/api/users"
+          "https://backend-nine-liard-28.vercel.app/api/users"
         );
         //const response = await fetch("http://localhost:5000/api/users");
         const responseData = await response.json();
